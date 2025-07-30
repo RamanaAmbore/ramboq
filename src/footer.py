c59940b8dc0c1215712e4f13ec7592d2c5d58079
Ramana Ambore <ramana.ambore@gmail.com>
2025-07-31 01:02:23 +0530

    moved footer constants to config.yaml

--- src/footer.py
+++ src/footer.py
@@ -1,24 +1,24 @@
 import streamlit as st
 
-from src.utils import constants
+from src.utils import config
 
 
 def footer():
-    footer_name = f"© {constants['footer_name']}"
+    footer_name = f"© {config['footer_name']}"
     with st.container(key='footer-container'):
         st.markdown(
             f"""
             <p class="footer-desktop">
                 {footer_name}
                 <span style='color:#007ACC;'> | </span>
-                {constants['footer_desktop_text2']}
+                {config['footer_desktop_text2']}
                 <span style='color:#007ACC;'> | </span>
-                {constants['footer_desktop_text3']}
+                {config['footer_desktop_text3']}
             </p>
             <p class="footer-mobile">
                 {footer_name}
                 <span style='color:#007ACC;'> | </span>
-                {constants['footer_mobile_text2']}
+                {config['footer_mobile_text2']}
             </p>
             """,
             unsafe_allow_html=True
