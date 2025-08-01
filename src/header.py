fbb39ae86a33e8e4e9f494047c4ba2a1b321003f
Ramana Ambore <ramana.ambore@gmail.com>
2025-08-01 20:44:47 +0530

    css cleanup - 3

--- src/header.py
+++ src/header.py
@@ -5,6 +5,18 @@ from src.utils import get_path
 
 nav_labels = ["About", "Market", "Performance", "Update", "Contact"]
 
+# Initialize login state
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+
+def create_logo(logo_width=300):
+    st.image(
+        get_path('logo.png'),
+        use_container_width="True",
+        width=logo_width
+    )
+
 
 def header():
     # Initialize session state
@@ -26,37 +38,36 @@ def create_navbar_desktop_container():
                                                          vertical_alignment="center")
 
         with logo_col:
-                st.image(
-                get_path('logo.png'),
-                use_container_width="False",
-                width=300)
-
+            create_logo()
         with menu_col:
-            with st.container(key="nav-buttons"):
-                nav_cols = st.columns([1,1,1.2,1.1,1.1], gap=None, vertical_alignment="center")
-
-                for i, label in enumerate(nav_labels):
-                    with nav_cols[i]:
-                        st.button(label, key=f'button-{label}-desktop')
+            nav_cols = st.columns([1, 1, 1.2, 1.1, 1.1], gap=None, vertical_alignment="center")
+            for i, label in enumerate(nav_labels):
+                with nav_cols[i]:
+                    st.button(label, type="tertiary", key=f'button-{label}-desktop')
         with login_col:
-            st.button("Logout" if st.session_state.logged_in else "Login")
+            st.button("Logout" if st.session_state.logged_in else "Login", on_click=toggle_login)
 
 
 def create_navbar_mobile_container():
     with st.container(key='navbar-mobile-container'):
-            col1, col2 = st.columns([4, .25],gap=None, vertical_alignment="center")  # Tune proportions
+        col1, col2 = st.columns([4, .25], gap=None, vertical_alignment="center")  # Tune proportions
 
-            with col1:
-                        st.image(
-                        get_path('logo.png'),
-                        use_container_width="True",
-                        width=300,
-                        )
-
-
-            with col2:
-                    with st.popover("☰", use_container_width=True):
-                        for label in nav_labels + ["Login"]:
-                            st.button(label,key=f'button-{label}-mobile')
+        with col1:
+            create_logo()
+        with col2:
+            with st.popover("☰", use_container_width=False):
+                for label in nav_labels + ["Login"]:
+                    if label == 'Login':
+                        st.button("Logout" if st.session_state.logged_in else "Login", on_click=toggle_login,
+                                  type="tertiary", key=f'button-{label}-mobile')
+                    else:
+                        st.button(label, type="tertiary", key=f'button-{label}-mobile')
 
 
+# Dummy function to simulate login/logout action
+def toggle_login():
+    st.session_state.logged_in = not st.session_state.logged_in
+    if st.session_state.logged_in:
+        st.toast("Logged in!")  # Optional feedback
+    else:
+        st.toast("Logged out!")
