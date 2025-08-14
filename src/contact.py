4026864a15985ade2d070791282b7445682540c3
Ramana Ambore <ramana.ambore@gmail.com>
2025-08-14 06:08:10 +0530

    contact page is updated

--- src/contact.py
+++ src/contact.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from src.utils import isd_codes  # YAML-loaded ISD codes
 from src.components import create_ruler_white
-
+from src.utils import config
 def contact(body_container):
     with body_container:
         with st.container(key='text-container'):
