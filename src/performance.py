6db35279787e9276fbc554bae496f28f2451d2b5
Ramana Ambore <ramana.ambore@gmail.com>
2025-08-21 22:30:21 +0530

    performance page updated - data & layout changes

--- src/performance.py
+++ src/performance.py
@@ -1,27 +1,36 @@
-import streamlit as st
-from src.helpers.utils import get_closing_date
-from src.helpers import kite_connect
 import pandas as pd
+import streamlit as st
+from src.helpers import kite_connect
+from src.helpers.utils import get_closing_date
+
 
 @st.cache_resource
 def get_books(dt):
     return kite_connect.get_books()
 
+
 def performance(body_container):
     with body_container:
-            df_books, df_positions, df_holdings, df_margin = get_books(get_closing_date())
+        df_books, df_positions, df_holdings, df_margin = get_books(get_closing_date())
 
-            tabs = st.tabs(["Portfolio", "Holdings", "Positions", "Cash"])
+        tabs = st.tabs(["Portfolio", "Holdings", "Positions", "Cash"])
 
-            with tabs[0]:
-                st.dataframe(df_books)
+        with tabs[0]:
+            st.dataframe(df_books, use_container_width=True,
+                         column_config={col: st.column_config.TextColumn(col, width="small") for col in
+                                        df_books.columns})
 
-            with tabs[1]:
-                st.write(df_holdings)
+        with tabs[1]:
+            st.write(df_holdings, use_container_width=True,
+                     column_config={col: st.column_config.TextColumn(col, width="small") for col in
+                                    df_holdings.columns})
 
-            with tabs[2]:
-                st.dataframe(df_positions)
+        with tabs[2]:
+            st.dataframe(df_positions, use_container_width=True,
+                         column_config={col: st.column_config.TextColumn(col, width="small") for col in
+                                        df_positions.columns})
 
-            with tabs[3]:
-                st.dataframe(df_margin)
-
+        with tabs[3]:
+            st.dataframe(df_margin, use_container_width=True,
+                         column_config={col: st.column_config.TextColumn(col, width="small") for col in
+                                        df_margin.columns})
