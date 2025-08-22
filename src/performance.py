3b78330d44578dc28843989a33bd5a1f3b9b5b56
Ramana Ambore <ramana.ambore@gmail.com>
2025-08-22 07:39:24 +0530

    changed cache_resource to cache_data

--- src/performance.py
+++ src/performance.py
@@ -11,26 +11,40 @@ def get_books(dt):
 
 def performance(body_container):
     with body_container:
+
+
+        # Apply custom styles
+        def style_dataframe(df):
+            return (
+                df.style
+                .set_properties(**{"background-color": "#fcfeff"})  # cell background
+                .set_table_styles(
+                    [{
+                        "selector": "thead th",
+                        "props": [("background-color", "#F2FBFF"), ("color", "red")]
+                    }]
+                )
+            )
+
+        tabs = st.tabs(["P & L", "Portfolio", "Holdings", "Positions", "Cash"])
         df_books, df_positions, df_holdings, df_margin = get_books(get_closing_date())
 
-        tabs = st.tabs(["Portfolio", "Holdings", "Positions", "Cash"])
-
         with tabs[0]:
-            st.dataframe(df_books, use_container_width=True,
-                         column_config={col: st.column_config.TextColumn(col, width="small") for col in
-                                        df_books.columns})
+            st.dataframe(style_dataframe(df_books), use_container_width=True,
+                         column_config={col: st.column_config.TextColumn(col, width="small") for col in df_books.columns})
 
         with tabs[1]:
-            st.write(df_holdings, use_container_width=True,
-                     column_config={col: st.column_config.TextColumn(col, width="small") for col in
-                                    df_holdings.columns})
+            st.dataframe(style_dataframe(df_books), use_container_width=True,
+                         column_config={col: st.column_config.TextColumn(col, width="small") for col in df_books.columns})
 
         with tabs[2]:
-            st.dataframe(df_positions, use_container_width=True,
-                         column_config={col: st.column_config.TextColumn(col, width="small") for col in
-                                        df_positions.columns})
+            st.dataframe(style_dataframe(df_holdings), use_container_width=True,
+                         column_config={col: st.column_config.TextColumn(col, width="small") for col in df_holdings.columns})
 
         with tabs[3]:
-            st.dataframe(df_margin, use_container_width=True,
-                         column_config={col: st.column_config.TextColumn(col, width="small") for col in
-                                        df_margin.columns})
+            st.dataframe(style_dataframe(df_positions), use_container_width=True,
+                         column_config={col: st.column_config.TextColumn(col, width="small") for col in df_positions.columns})
+
+        with tabs[4]:
+            st.dataframe(style_dataframe(df_margin), use_container_width=True,
+                         column_config={col: st.column_config.TextColumn(col, width="small") for col in df_margin.columns})
