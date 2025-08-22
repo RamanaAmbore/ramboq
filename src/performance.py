3f8dc322a2623f87aa941de55cfe0a0a91625154
Ramana Ambore <ramana.ambore@gmail.com>
2025-08-22 12:42:04 +0530

    dataframe format changes for performance tab

--- src/performance.py
+++ src/performance.py
@@ -4,15 +4,13 @@ from src.helpers import kite_connect
 from src.helpers.utils import get_closing_date
 
 
-@st.cache_data (show_spinner="Connecting to broker platform and loading data…")
+@st.cache_data(show_spinner="Connecting to broker platform and loading data…")
 def get_books(dt):
     return kite_connect.get_books()
 
 
 def performance(body_container):
     with body_container:
-
-
         # Apply custom styles
         def style_dataframe(df):
             return (
@@ -31,21 +29,27 @@ def performance(body_container):
 
         with tabs[0]:
             st.dataframe(style_dataframe(df_books), use_container_width=True,
-                         column_config={col: st.column_config.TextColumn(col, width="small") for col in df_books.columns})
+                         column_config={col: st.column_config.TextColumn(col, width="small") for col in
+                                        df_books.columns})
 
         with tabs[1]:
             st.dataframe(style_dataframe(df_books), use_container_width=True,
-                         column_config={col: st.column_config.TextColumn(col, width="small") for col in df_books.columns})
+                         column_config={col: st.column_config.TextColumn(col, width="small") for col in
+                                        df_books.columns})
 
         with tabs[2]:
             st.dataframe(style_dataframe(df_holdings), use_container_width=True,
-                         column_order=("Symbol",	"P&L", "Δ Val", "Inv Val", "Cur Val", "Qty",	"I Price",		"C Price",	"ΔPrice",	"ΔPrice%",	"Date",	"Account"			),
-                         column_config={col: st.column_config.TextColumn(col, width=None ) for col in df_holdings.columns})
+                         column_order=("Symbol", "P&L", "Cur Val", "Inv Val", "Δ Val", "Qty", "I Price", "C Price",
+                                       "ΔPrice", "ΔPrice%", "Date", "Account"),
+                         column_config={col: st.column_config.TextColumn(col, width=None) for col in
+                                        df_holdings.columns})
 
         with tabs[3]:
             st.dataframe(style_dataframe(df_positions), use_container_width=True,
-                         column_config={col: st.column_config.TextColumn(col, width="small") for col in df_positions.columns})
+                         column_config={col: st.column_config.TextColumn(col, width="small") for col in
+                                        df_positions.columns})
 
         with tabs[4]:
             st.dataframe(style_dataframe(df_margin), use_container_width=True,
-                         column_config={col: st.column_config.TextColumn(col, width="small") for col in df_margin.columns})
+                         column_config={col: st.column_config.TextColumn(col, width="small") for col in
+                                        df_margin.columns})
